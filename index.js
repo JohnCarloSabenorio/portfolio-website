de7948bc944d3157a8c7c73806e894b975d5c039
John Carlo Sabenorio <johncarlosabenorio07@gmail.com>
2024-09-20 21:07:10 +0800

    changed handleResize width to 1000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ checkbox = document.getElementById("check");
 
 
 function handleResize(){
-    if(window.innerWidth > 680){
+    if(window.innerWidth > 1000){
         checkbox.checked = false;
     }
     
