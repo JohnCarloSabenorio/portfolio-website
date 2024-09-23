6e0e47e8abf9cef76d67e1691c94a458d7b0cd7c
John Carlo Sabenorio <johncarlosabenorio07@gmail.com>
2024-09-23 20:03:54 +0800

    added responsive navbar

---
+++ index.js
@@ -0,0 +1,5 @@
+// const navMenuBtn = document.getElementById("navmenu-btn");
+// const mobileNav = document.querySelector(".mobile-nav");
+// navMenuBtn.addEventListener("click", (e)=>{
+//     mobileNav.style.left = "0";
+// });
